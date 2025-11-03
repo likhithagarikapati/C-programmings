8d620c800ef75324818659caa272db86a728c1a0
likhithagarikapati <likhithagarikapati434@gmail.com>
2025-11-03 13:23:06 +0530

    Add files via upload

---
+++ big of 3 else if.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("entera,b,c values");
+	scanf("%d%d%d",&a,&b,&c);
+	if(a>b&&a>c)
+	printf("b is big");
+	else
+	printf("c is big");
+	return 0;
+}
